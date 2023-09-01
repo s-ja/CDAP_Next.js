a50444c40b918d8a042cda79c96ef387105d56f4
s-ja <dkstmdwl0615@naver.com>
2023-09-01 10:55:48 +0900

    6강 진행중

--- fresh/app/list/page.tsx
+++ fresh/app/list/page.tsx
@@ -1,12 +1,24 @@
+import Image from "next/image";
+
 export default function List() {
-  let product = ["tomaotes", "pasta", "banana", "cherry"];
+  let product = ["tomatoes", "pasta", "coconut"];
   return (
     <div>
       <h4 className="title">product list</h4>
       {product.map((a, i) => {
         return (
-          <div className="food">
-            <h4>{product[i]}</h4>
+          <div className="food" key={i}>
+            <div className="food_img">
+              <Image
+                src={`https://codingapple.com/wp-content/uploads/2023/01/food${i}.png`}
+                width={500}
+                height={400}
+                layout="responsive"
+                alt={`${i}'s img`}
+              />
+            </div>
+            <img src="" alt="" />
+            <h4>{a}</h4>
           </div>
         );
       })}
