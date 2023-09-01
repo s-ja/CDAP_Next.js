cf5b127c842f5f34450863032fc4d990c800c61a
seungji <dkstmdwl0615@naver.com>
2023-09-01 22:13:45 +0900

    6강 완료

--- fresh/app/cart/page.tsx
+++ fresh/app/cart/page.tsx
@@ -2,6 +2,16 @@ export default function Cart() {
   return (
     <div>
       <h4 className="title">cart</h4>
+      <div className="cart_item">
+        <p>product name</p>
+        <p>$40</p>
+        <p>1 ea</p>
+      </div>
+      <div className="cart_item">
+        <p>product name</p>
+        <p>$40</p>
+        <p>1 ea</p>
+      </div>
     </div>
   );
 }
