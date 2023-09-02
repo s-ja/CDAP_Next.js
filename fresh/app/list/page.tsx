93d3c1adaa1c6f7a1dd3a701541bbc1c101a5513
seungji <dkstmdwl0615@naver.com>
2023-09-02 21:28:41 +0900

    10강 완료, 파트 1 종료

--- fresh/app/list/page.tsx
+++ fresh/app/list/page.tsx
@@ -1,11 +1,12 @@
 "use client";
 
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function List() {
   let product = ["tomatoes", "pasta", "coconut"];
-  let [quantity, setQuantity] = useState(0);
+  let [quantity, setQuantity] = useState(Array(product.length).fill(0));
+
   return (
     <div>
       <h4 className="title">product list</h4>
@@ -15,24 +16,32 @@ export default function List() {
             <div className="food_img">
               <Image
                 src={`https://codingapple.com/wp-content/uploads/2023/01/food${i}.png`}
-                width={500}
-                height={400}
-                layout="responsive"
+                width={300}
+                height={300}
+                // sizes="100vw"
+                style={{
+                  width: "100%",
+                  height: "auto",
+                }}
                 alt={`${i}'s img`}
               />
             </div>
             <h4>{a}</h4>
             <button
               onClick={() => {
-                setQuantity(quantity - 1);
+                let newQuantity = [...quantity];
+                newQuantity[i] = Math.max(newQuantity[i] - 1, 0);
+                setQuantity(newQuantity);
               }}
             >
               -
             </button>
-            <span>{quantity}</span>
+            <span>{quantity[i]}</span>
             <button
               onClick={() => {
-                setQuantity(quantity + 1);
+                let newQuantity = [...quantity];
+                newQuantity[i] = newQuantity[i] + 1;
+                setQuantity(newQuantity);
               }}
             >
               +
