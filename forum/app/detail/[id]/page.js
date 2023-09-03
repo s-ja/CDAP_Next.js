aa324508715be4a6c60d8c77192552ec9100d39e
seungji <dkstmdwl0615@naver.com>
2023-09-03 17:47:12 +0900

    4강 종료

---
+++ forum/app/detail/[id]/page.js
@@ -0,0 +1,19 @@
+import { connectDB } from "@/util/database";
+import { ObjectId } from "bson";
+
+export default async function Detail(props) {
+  const db = (await connectDB).db("forum");
+  let result = await db
+    .collection("post")
+    .findOne({ _id: new ObjectId(props.params.id) });
+
+  console.log(props);
+
+  return (
+    <div>
+      <h4>post</h4>
+      <h4>{result.title}</h4>
+      <p>{result.content}</p>
+    </div>
+  );
+}
--- forum/app/list/page.js
+++ forum/app/list/page.js
@@ -1,19 +1,24 @@
 import { connectDB } from "@/util/database";
+import Link from "next/link";
 
 export default async function List() {
   const db = (await connectDB).db("forum");
   let result = await db.collection("post").find().toArray();
 
-  // console.log(result);
+  // console.log(result[0]._id);
 
   return (
     <div className="list_bg">
       {result.map((a, i) => {
+        console.log(Object.keys(a._id));
+        const idString = a._id.toString(); // ObjectId를 문자열로 변환
         return (
-          <div className="list_item">
-            <h4>{a.title}</h4>
-            <p>{a.content}</p>
-          </div>
+          <Link href={`/detail/${idString}`}>
+            <div className="list_item" key={i}>
+              <h4>{a.title}</h4>
+              <p>{a.content}</p>
+            </div>
+          </Link>
         );
       })}
     </div>
