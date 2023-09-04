5eb8918079fb53cf57ba9cc013ed809b34d8f35b
seungji <dkstmdwl0615@naver.com>
2023-09-04 11:39:13 +0900

    7강 진행중

--- forum/app/write/page.js
+++ forum/app/write/page.js
@@ -1,10 +1,10 @@
 export default function Write() {
   return (
-    <div>
+    <div className="p-20">
       <h4>write post</h4>
-      <form action="/api/test" method="POST">
-        <input type="text" name="title" />
-        <input type="text" name="content" />
+      <form action="/api/post/new" method="POST">
+        <input type="text" name="title" placeholder="post title" />
+        <input type="text" name="content" placeholder="post content" />
         <button type="submit">submit</button>
       </form>
     </div>
---
+++ forum/pages/api/post/new.js
@@ -0,0 +1,22 @@
+import { connectDB } from "@/util/database";
+// import { redirect } from "next/navigation";
+
+export default async function handler(request, response) {
+  if (request.method == "POST") {
+    if (request.body.title == "") {
+      return response.status(500).json("empty title");
+    } else if (request.body.content == "") {
+      return response.status(500).json("empty content");
+    } else {
+      try {
+        const db = (await connectDB).db("forum");
+        let result = await db.collection("post").insertOne(request.body);
+        return response.status(200).redirect("/list");
+        // response.status(200).json("completed");
+        // response.status(200).redirect("/list");
+      } catch (error) {
+        return response.status(500).json("server error");
+      }
+    }
+  }
+}
