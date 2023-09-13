add813d41f7d5c2830042e5ff2d642a3725563fd
s-ja <dkstmdwl0615@naver.com>
2023-09-13 17:41:31 +0900

    12강 종료

--- forum/app/list/ListItem.js
+++ forum/app/list/ListItem.js
@@ -16,27 +16,44 @@ export default function ListItem({ result }) {
             <Link href={`/edit/${result[i]._id}`}>✏️ edit</Link>
             <span
               onClick={(e) => {
-                fetch("/api/post/delete", {
-                  method: "DELETE",
-                  body: result[i]._id,
-                })
+                // fetch("/api/post/delete", {
+                //   method: "DELETE",
+                //   body: result[i]._id,
+                // })
+                //   .then((r) => {
+                //     if (r.status == 200) {
+                //       return r.json();
+                //     } else {
+                //       //서버가 에러코드전송시 실행할코드
+                //     }
+                //   })
+                //   .then((r) => {
+                //     // window.location.reload();
+                //     // console.log(r);
+                //     e.target.parentElement.style.opacity = 0;
+                //     setTimeout(() => {
+                //       e.target.parentElement.style.display = "none";
+                //     }, 1000);
+                //   })
+                //   .catch((error) => {
+                //     //인터넷문제 등으로 실패시 실행할코드
+                //     console.log(error);
+                //   });
+                // fetch("/api/test?name=asj&age=29");
+                // fetch("/api/test/123");
+                fetch(`/api/test/${idString}`, { method: "DELETE" })
                   .then((r) => {
-                    if (r.status == 200) {
-                      return r.json();
+                    if (r.ok) {
+                      return r.text();
                     } else {
-                      //서버가 에러코드전송시 실행할코드
+                      throw new Error("Error");
                     }
                   })
                   .then((r) => {
-                    // window.location.reload();
-                    // console.log(r);
-                    e.target.parentElement.style.opacity = 0;
-                    setTimeout(() => {
-                      e.target.parentElement.style.display = "none";
-                    }, 1000);
+                    console.log(r);
+                    window.location.reload();
                   })
                   .catch((error) => {
-                    //인터넷문제 등으로 실패시 실행할코드
                     console.log(error);
                   });
               }}
