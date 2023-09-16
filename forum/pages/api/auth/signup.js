ad935efbc4afdaeac5554c112ed9a1b735d0100b
seungji <dkstmdwl0615@naver.com>
2023-09-16 18:28:03 +0900

    17강 응용1번 : 회원 가입시 빈 값을 입력할 경우 거절 기능

--- forum/pages/api/auth/signup.js
+++ forum/pages/api/auth/signup.js
@@ -2,11 +2,17 @@ import { connectDB } from "@/util/database";
 import bcrypt from "bcrypt";
 
 export default async function handler(request, response) {
+  console.log(request.body);
   if (request.method == "POST") {
+    if (request.body.name == "") {
+      response.status(500).json("empty name");
+    } else if (request.body.email == "") {
+      response.status(500).json("empty email");
+    } else if (request.body.password == "") {
+      response.status(500).json("empty password");
+    }
     let hash = await bcrypt.hash(request.body.password, 10);
     request.body.password = hash;
-
-    
 
     let db = (await connectDB).db("forum");
     await db.collection("user_cred").insertOne(request.body);
